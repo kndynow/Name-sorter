326e926e580bc22d98d6ceb3e84d9534cd119756
kndynow <139381069+kndynow@users.noreply.github.com>
2024-10-02 22:20:35 +0200

    Add: Lägg till meny och metoder för välja
    Implementerade en meny där användaren kan välja vilken del av programmet
    som ska köras samt avsluta.

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -14,35 +14,102 @@ class Program
     static void Main(string[] args)
     {
         //TODO:Presentera meny
+        do
+        {
+            DisplayMenu(["Visa namn osorterat",
+            "Visa namn i bokstavsordning",
+            "Sök",
+            "Sortera namn i bokstavsordning",
+            "Avsluta"]);
 
-        //TODO:Formatera kod så den blir mer läsbar:
-        List<string> names = new List<string> { "Anna", "John", "Alice", "Björn", "Rakel", "Örjan", "Zeke", "Ahmed", "Hamse", "Linda", "Fehrvats", "Karin", };
-        //TODO:Implementera metod som presenterar namn osorterade
-        Console.WriteLine("Original list:");
-        foreach (var name in names)
+            int userInput = GetUserInput();
+
+            switch (userInput)
+            {
+                //Visa namn osorterade
+                case 1:
+                Console.Clear();
+                Console.WriteLine("Visa namn");
+                Console.ReadLine();
+                    break;
+                //Visa namn i bokstavsordning
+                case 2:
+                Console.Clear();
+                Console.WriteLine("Visa namn sorterat");
+                Console.ReadLine();
+                    break;
+                //Sök
+                case 3:
+                Console.Clear();
+                Console.WriteLine("Sök");
+                Console.ReadLine();
+                    break;
+                //Sortera
+                case 4:
+                Console.Clear();
+                Console.WriteLine("Sortera");
+                Console.ReadLine();
+                    break;
+                //Avsluta
+                case 5:
+                Environment.Exit(0);
+                    break;
+
+                default:
+                Console.Clear();
+                    Console.WriteLine("Felaktig inmatning, försökt igen\nTryck Enter för att fortsätta...");
+                    break;
+            }
+
+        } while (true);
+        // //TODO:Formatera kod så den blir mer läsbar:
+        // List<string> names = new List<string> { "Anna", "John", "Alice", "Björn", "Rakel", "Örjan", "Zeke", "Ahmed", "Hamse", "Linda", "Fehrvats", "Karin", };
+        // //TODO:Implementera metod som presenterar namn osorterade
+        // Console.WriteLine("Original list:");
+        // foreach (var name in names)
+        // {
+        //     Console.WriteLine(name);
+        // }
+        // //TODO:Försök lösa så att Sort() sorterar efter svenska alfabetet.
+        // names.Sort();
+        // Console.WriteLine("\nSorted list:");
+        // foreach (var name in names)
+        // {
+        //     Console.WriteLine(name);
+        // }
+        // //TODO:Implementera metod för sökning med felhantering så som felaktig inmatning eller om listan är tom.
+        // Console.WriteLine("\nEnter name to search:");
+        // //TODO:Här kommer jag implementera felhantering så att användaren inte kan mata in whitespace eller null
+        // string searchName = Console.ReadLine();
+
+        // if (names.Contains(searchName))
+        // {
+        //     Console.WriteLine($"{searchName} is in the list");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{searchName} is not in the list.");
+        // }
+        // Console.ReadKey();
+
+
+    }
+
+    public static void DisplayMenu(string[] menuItems)
+    {
+        Console.Clear();
+        for (int i = 0; i < menuItems.Length; i++)
         {
-            Console.WriteLine(name);
+            Console.WriteLine($"{(i) + 1} - {menuItems[i]}");
         }
-        //TODO:Försök lösa så att Sort() sorterar efter svenska alfabetet.
-        names.Sort();
-        Console.WriteLine("\nSorted list:");
-        foreach (var name in names)
+    }
+    //Kontrollerar att användaren matar in ett numreriskt värde
+    public static int GetUserInput()
+    {
+        if (!int.TryParse(Console.ReadLine(), out int input))
         {
-            Console.WriteLine(name);
+            Console.WriteLine("Ogiltig inmatning. Försök igen");
         }
-        //TODO:Implementera metod för sökning med felhantering så som felaktig inmatning eller om listan är tom.
-        Console.WriteLine("\nEnter name to search:");
-        //TODO:Här kommer jag implementera felhantering så att användaren inte kan mata in whitespace eller null
-        string searchName = Console.ReadLine();
-        
-        if (names.Contains(searchName))
-        {
-            Console.WriteLine($"{searchName} is in the list");
-        }
-        else
-        {
-            Console.WriteLine($"{searchName} is not in the list.");
-        }
-        Console.ReadKey();
+        return input;
     }
 }
